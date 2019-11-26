fa485b4516741ef7627bd7bb47ba21b26b9803b9
Gary Clark <gclark04432@live.co.uk>
2019-11-26 17:44:41 +0000

    test-river-instantiation-with-fish-in-array

--- river.rb
+++ river.rb
@@ -2,9 +2,13 @@ class River
 
 attr_reader :name
 
-  def initialize(name)
+  def initialize(name, fish_population)
     @name = name
+    @fish_population = fish_population
   end
 
+  def fish_count()
+    return @fish_population.length()
+  end
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,17 +1,24 @@
 require ('minitest/autorun')
 require ('minitest/reporters')
 require_relative ('../river')
+require_relative('../fish')
+require_relative('../bear')
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestRiver < Minitest::Test
 
   def setup
-    @river = River.new("Amazon")
+    @river = River.new("Amazon", [@fish, @fish, @fish] )
+    @fish = Fish.new("Freddy")
   end
 
   def test_river_has_name
     assert_equal("Amazon", @river.name())
+  end
+
+  def test_river_starts_with_three_fish
+    assert_equal(3, @river.fish_count())
   end
 
 end
